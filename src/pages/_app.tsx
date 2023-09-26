886484988822d925d96694cbb5d8ae075fc2c831
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-27 01:02:47 +0530

    Added New Dependencies  - NextUI  - ShadcnUI  - Next-Themes
    Added New Components
     - ThemeSwitcher

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import { type AppType } from "next/app";
 
 import { api } from "@/utils/api";
+import { NextUIProvider } from "@nextui-org/react";
 
 import "@/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <main className="text-foreground bg-background dark">
+          <Component {...pageProps} />
+        </main>
+      </ThemeProvider>
+    </NextUIProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
