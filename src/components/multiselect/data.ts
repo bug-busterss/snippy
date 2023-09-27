b892121d950bec83ec8f5a266e84427e0b847380
Bhavesh Patil <96092183+Uraniumking007@users.noreply.github.com>
2023-09-27 16:52:18 +0530

    Create Snip UI

---
+++ src/components/multiselect/data.ts
@@ -0,0 +1,53 @@
+import React from "react";
+
+export const VisilityOptions = [
+  {
+    label: "Public",
+    value: "public",
+    checked: false,
+  },
+  {
+    label: "Private",
+    value: "private",
+    checked: false,
+  },
+  {
+    label: "Protected",
+    value: "protected",
+    checked: false,
+  },
+];
+export const languages = [
+  {
+    label: "Public",
+    value: "public",
+    checked: false,
+  },
+  {
+    label: "Private",
+    value: "private",
+    checked: false,
+  },
+  {
+    label: "Protected",
+    value: "protected",
+    checked: false,
+  },
+];
+export const tagsOptions = [
+  {
+    label: "Public",
+    value: "public",
+    checked: false,
+  },
+  {
+    label: "Private",
+    value: "private",
+    checked: false,
+  },
+  {
+    label: "Protected",
+    value: "protected",
+    checked: false,
+  },
+];
---
+++ src/components/multiselect/multi-select.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+import { Select, SelectItem, type Selection } from "@nextui-org/react";
+import { VisilityOptions } from "./data";
+
+interface MultiSelectProps {
+  label: string;
+  selected: Selection;
+  setValues: React.Dispatch<React.SetStateAction<Selection>>;
+  options: { label: string; value: string; checked?: boolean }[];
+}
+
+export default function MultiSelect({
+  label,
+  selected,
+  setValues,
+  options,
+}: MultiSelectProps) {
+  // const [selected, setValues] = React.useState<Selection>(
+  //   new Set(["cat", "dog"]),
+  // );
+
+  return (
+    <div className="flex w-full flex-col gap-2">
+      <Select
+        label={label}
+        selectionMode="single"
+        placeholder="Select an animal"
+        selectedKeys={selected}
+        className="w-full"
+        onSelectionChange={setValues}
+      >
+        {options.map((option) => (
+          <SelectItem key={option.value} value={option.value}>
+            {option.label}
+          </SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
