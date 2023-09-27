2d9881b097c0ce4f214fe1e43e1ef2f64b5a0682
UraniumKing <bhaveshpatil918@gmail.com>
2023-09-28 02:39:32 +0530

    Added Protected procedures for snip

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { snip } from "@/server/api/routers/snip";
 import { createTRPCRouter } from "@/server/api/trpc";
+import { protectedSnip } from "@/server/api/routers/protected-snips";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "@/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   snip,
+  protectedSnip,
 });
 
 // export type definition of API
