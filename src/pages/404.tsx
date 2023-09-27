f33b0ec817d5f521ac5d0caaa46db562f3a71f1f
Adnanarodiya <aiarodiya07@gmail.com>
2023-09-28 00:54:09 +0530

    changes in Singin btn of error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { Button, useDisclosure } from "@nextui-org/react";
 import { useUser } from "@supabase/auth-helpers-react";
 import React from "react";
@@ -11,7 +12,7 @@ export default function NotFoundPage() {
     <>
       <main className="flex h-screen w-full flex-col items-center justify-center bg-black">
         <img
-          className="mb-6"
+          className="mb-6 rounded-xl"
           src="https://imgs.search.brave.com/4456AxXu6O3bsEvXCKxEjJ6Bl4gwoVe8HtCiBQyBFFE/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0Lzk5LzcyLzA5/LzM2MF9GXzQ5OTcy/MDk5NV9zZm1Vb1dv/bTUwa3gyNUNZY09p/WUxWeEJPc3RkYjVq/ai5qcGc"
           alt="spinyy"
           width={250}
@@ -23,14 +24,10 @@ export default function NotFoundPage() {
           {user ? `you logged in as {user.email}` : `you are not logged in`}
         </p>
 
-        <Button
-          onPress={loginModal.onOpen}
-          color="primary"
-          className="mb-6 w-56"
-        >
+        <Button onPress={loginModal.onOpen} color="primary" className=" w-56">
           {user ? `Sign in as different user` : `Sign in`}
-          Sign in as different user
         </Button>
+        <LoginModal {...loginModal} />
       </main>
     </>
   );
