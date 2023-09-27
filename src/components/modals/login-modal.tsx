a8b35cb42cc191b0d504e389c1b7364ad529f51c
Adnanarodiya <aiarodiya07@gmail.com>
2023-09-27 20:37:40 +0530

    login modal completed

--- src/components/modals/login-modal.tsx
+++ src/components/modals/login-modal.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "@nextui-org/react";
 import {
   Modal,
   ModalContent,
@@ -8,7 +8,9 @@ import {
   ModalFooter,
   Button,
   type useDisclosure,
+  Input,
 } from "@nextui-org/react";
+import { Github } from "lucide-react";
 
 export default function LoginModal({
   isOpen,
@@ -20,37 +22,53 @@ export default function LoginModal({
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">
+              <ModalHeader className=" flex flex-col gap-1 text-center">
                 Sign in to your account
               </ModalHeader>
               <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
+                <div className="text-center"></div>
                 <Button color="primary" onPress={onClose}>
-                  Action
+                  <Github size={22} />
+                  Sign in with GitHub
                 </Button>
-              </ModalFooter>
+                <p className="text-center text-sm">or continnue with email</p>
+
+                <div className="mb-6 flex w-full flex-wrap items-end gap-4 md:mb-0 md:flex-nowrap">
+                  <Input
+                    className="outline-none"
+                    key={"outside"}
+                    type="email"
+                    label="Email Address"
+                    labelPlacement={"outside"}
+                    placeholder="your@email.com"
+                  />
+                </div>
+                <div className="mb-6 flex w-full flex-wrap items-end gap-4 md:mb-0 md:flex-nowrap">
+                  <Input
+                    className="outline-none"
+                    key={"outside"}
+                    type="Password"
+                    label="Password"
+                    labelPlacement={"outside"}
+                    placeholder="Password"
+                  />
+                </div>
+                <Link href="#" className="text-sm" underline="hover">
+                  forgrt password?
+                </Link>
+                <Button color="primary" onPress={onClose}>
+                  Sign In
+                </Button>
+                <div className="flex justify-center gap-2">
+                  <p className="text-center text-sm">
+                    Do you have an account yet?{" "}
+                  </p>
+                  <Link href="#" className="text-sm" underline="hover">
+                    Sign Up
+                  </Link>
+                </div>
+              </ModalBody>
+              <ModalFooter />
             </>
           )}
         </ModalContent>
