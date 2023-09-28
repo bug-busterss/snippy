9dd6eadbace2007ad8d64ae83e238310c43cd4c0
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2023-09-28 11:38:13 +0530

    Add messages for private snips

--- src/pages/s/[slug].tsx
+++ src/pages/s/[slug].tsx
@@ -19,50 +19,62 @@ export default function SnipDisplay(
   props: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) {
   const router = useRouter();
-  const { data: snip } = api.snip.getOne.useQuery({
+  const {
+    data: snip,
+    error,
+    isLoading,
+  } = api.snip.getOne.useQuery({
     slug: router.query.slug as string,
   });
 
   return (
     <div>
-      {snip && (
-        <div className="container mx-auto">
-          <div className="flex flex-col items-center justify-center gap-3">
-            <h1 className="text-3xl font-medium">{snip.title}</h1>
-            <h3 className="text-xl">
-              Created: {dayjs(snip.createdAt).format("D MMM YYYY")}
-            </h3>
-            {snip.updatedAt > snip.createdAt && (
+      <div className="container mx-auto">
+        {isLoading && (
+          <h1 className="text-center text-3xl font-medium">Loading...</h1>
+        )}
+        {error && (
+          <h1 className="text-center text-3xl font-medium">{error.message}</h1>
+        )}
+        {snip && (
+          <>
+            <div className="flex flex-col items-center justify-center gap-3">
+              <h1 className="text-3xl font-medium">{snip.title}</h1>
               <h3 className="text-xl">
-                Updated: {dayjs(snip.updatedAt).format("D MMM YYYY")}
+                Created: {dayjs(snip.createdAt).format("D MMM YYYY")}
               </h3>
-            )}
-            <div className="flex items-center">
-              <Chip color="warning" variant="flat">
-                {snip.language}
-              </Chip>
-              <Dot />
-              <Chip color="secondary">{snip.views} views</Chip>
+              {snip.updatedAt > snip.createdAt && (
+                <h3 className="text-xl">
+                  Updated: {dayjs(snip.updatedAt).format("D MMM YYYY")}
+                </h3>
+              )}
+              <div className="flex items-center">
+                <Chip color="warning" variant="flat">
+                  {snip.language}
+                </Chip>
+                <Dot />
+                <Chip color="secondary">{snip.views} views</Chip>
+              </div>
             </div>
-          </div>
-          <div className="relative">
-            <div className="absolute right-5 top-2">
-              <SnipActions slugId={snip.id} />
+            <div className="relative">
+              <div className="absolute right-5 top-2">
+                <SnipActions slugId={snip.id} />
+              </div>
+              <CodeMirror
+                value={snip.content}
+                height="384px"
+                theme={xcodeDark}
+                className="mt-5 text-xl"
+                editable={false}
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+                extensions={[loadLanguage(snip.language)]}
+              />
             </div>
-            <CodeMirror
-              value={snip.content}
-              height="384px"
-              theme={xcodeDark}
-              className="mt-5 text-xl"
-              editable={false}
-              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-              // @ts-ignore
-              // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-              extensions={[loadLanguage(snip.language)]}
-            />
-          </div>
-        </div>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 }
