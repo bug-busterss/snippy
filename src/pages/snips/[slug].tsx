4b03674f7e839048d1546880c229cebf2a2b051e
Usman S. (Max Programming) <ui.sabuwala@gmail.com>
2023-09-28 07:50:16 +0530

    Showing error message on invalid slug

--- src/pages/snips/[slug].tsx
+++ src/pages/snips/[slug].tsx
@@ -13,45 +13,76 @@ import { Dot } from "lucide-react";
 
 export default function SnipDisplay() {
   const router = useRouter();
-  const { data: snip } = api.snip.getOne.useQuery({
+  const { data: snip, error } = api.snip.getOne.useQuery({
     slug: router.query.slug as string,
   });
 
   return (
     <div>
-      {snip && (
-        <div className="container mx-auto">
-          <div className="flex flex-col items-center justify-center gap-3">
-            <h1 className="text-3xl font-medium">{snip.title}</h1>
-            <h3 className="text-xl">
-              Created: {dayjs(snip.createdAt).format("D MMM YYYY")}
-            </h3>
-            {snip.updatedAt > snip.createdAt && (
+      <div className="container mx-auto">
+        {error && (
+          <h1 className="text-center text-3xl font-medium">{error.message}</h1>
+        )}
+        {snip && (
+          <>
+            <div className="flex flex-col items-center justify-center gap-3">
+              <h1 className="text-3xl font-medium">{snip.title}</h1>
               <h3 className="text-xl">
-                Updated: {dayjs(snip.updatedAt).format("D MMM YYYY")}
+                Created: {dayjs(snip.createdAt).format("D MMM YYYY")}
               </h3>
-            )}
-            <div className="flex items-center">
-              <Chip color="warning" variant="flat">
-                {snip.language}
-              </Chip>
-              <Dot />
-              <Chip color="secondary">{snip.views} views</Chip>
+              {snip.updatedAt > snip.createdAt && (
+                <h3 className="text-xl">
+                  Updated: {dayjs(snip.updatedAt).format("D MMM YYYY")}
+                </h3>
+              )}
+              <div className="flex items-center">
+                <Chip color="warning" variant="flat">
+                  {snip.language}
+                </Chip>
+                <Dot />
+                <Chip color="secondary">{snip.views} views</Chip>
+              </div>
             </div>
-          </div>
-          <CodeMirror
-            value={snip.content}
-            height="384px"
-            theme={xcodeDark}
-            className="mt-5 text-xl"
-            editable={false}
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-            extensions={[loadLanguage(snip.language)]}
-          />
-        </div>
-      )}
+            <CodeMirror
+              value={snip.content}
+              height="384px"
+              theme={xcodeDark}
+              className="mt-5 text-xl"
+              editable={false}
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+              extensions={[loadLanguage(snip.language)]}
+            />
+
+            <div>
+              <h1 className="text-3xl font-medium">Comments</h1>
+              <div className="flex flex-col gap-2">
+                {snip.comments.map((comment) => (
+                  <div className="flex flex-col gap-1">
+                    <div className="flex items-center gap-2">
+                      <img
+                        src={comment.user.avatar}
+                        alt="avatar"
+                        className="h-8 w-8 rounded-full"
+                      />
+                      <div className="flex flex-col gap-1">
+                        <h3 className="text-xl font-medium">
+                          {comment.user.username}
+                        </h3>
+                        <h4 className="text-lg">{comment.content}</h4>
+                      </div>
+                    </div>
+                    <h5 className="text-sm text-gray-500">
+                      {dayjs(comment.createdAt).format("D MMM YYYY")}
+                    </h5>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
