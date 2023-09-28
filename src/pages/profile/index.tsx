c29ccc26b5c29003e114b642c67dd2796b576db0
Tejash Patel <pateltejash429@gmail.com>
2023-09-28 05:40:44 +0530

    removed unwanted styles

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -26,8 +26,8 @@ const snips = [
     visibility: "private",
   },
   {
-    title: "Snippet",
-    language: "C++",
+    title: "This is a very long snip title",
+    language: "java",
     visibility: "private",
   },
   {
@@ -46,22 +46,22 @@ export default function UserProfile() {
   const user = useUser();
   return (
     <div className="h-full bg-black">
-      <div className="flex flex-col items-center justify-center gap-4 ">
+      <div className="flex flex-col items-center justify-center gap-6 pt-6">
         <Image
           width={100}
           height={100}
           src="https://imgs.search.brave.com/z-3_VMVbo5k1c4nYN-MGQSo7jUle-HYqNAS5ED1Soag/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9kMnFw/MHNpb3RsYTc0Ni5j/bG91ZGZyb250Lm5l/dC9pbWcvdXNlLWNh/c2VzL3Byb2ZpbGUt/cGljdHVyZS90ZW1w/bGF0ZV8wLmpwZw"
           alt="User Img"
-          className="mb-3 rounded-full"
+          className=" rounded-full"
         />
         <h3 className="text-2xl"> {user && user.user_metadata.name}</h3>
 
-        <p className="mb-2 text-gray-400">@{user && user.id}</p>
+        <p className=" text-gray-400">@{user && user.id}</p>
 
-        <p className=" border border-gray-400"></p>
+        <p className="border border-gray-400"></p>
       </div>
 
-      <div className="flex flex-wrap justify-center gap-6  ">
+      <div className="flex  flex-wrap justify-center  gap-6">
         {snips.map((snip, key) => (
           <SnipCard snip={snip} key={key} />
         ))}
